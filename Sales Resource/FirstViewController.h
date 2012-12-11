529cac55d3a4e228493bc3a87b0297e21cc9eca8
ninnemana <ninnemana@gmail.com>
2012-12-10 21:42:00 -0600

    init

---
+++ Sales Resource/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Sales Resource
+//
+//  Created by Alex Ninneman on 12/10/12.
+//  Copyright (c) 2012 CURT Manufacturing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
